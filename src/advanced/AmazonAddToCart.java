10510727c41e2392011e1c0402e48fb716590684
SatwaTech59 <149996412+SatwaTech59@users.noreply.github.com>
2023-11-06 12:34:22 +0530

    added new package and class for amazon

---
+++ src/advanced/AmazonAddToCart.java
@@ -0,0 +1,13 @@
+package advanced;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonAddToCart {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
